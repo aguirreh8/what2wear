d9eba55d14a4f4fce28a0cbacfca71122dc7fd0a
unknown <aguirreh8@gmail.com>
2018-10-27 16:53:07 -0400

    Try global variable WeatherType

--- server.js
+++ server.js
@@ -9,39 +9,41 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 const ebay = new Ebay({
-	app_id: process.env.EBAY_APP_ID
+    app_id: process.env.EBAY_APP_ID
 });
+let weatherType = "";
 
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "index-design.html"));
+    res.sendFile(path.join(__dirname, "index-design.html"));
 
 });
 
 app.get("/api/:zip?", function(req, res) {
-	weather.find({search: req.params.zip, degreeType: 'F'}, function(err, result) {
-	  if(err) console.log(err);
-	 
-	  // const weatherResults = JSON.stringify(result, null, 1);
-	  return res.json(result);
-	});
+    weather.find({ search: req.params.zip, degreeType: 'F' }, function(err, result) {
+        if (err) console.log(err);
+        // weatherType = result[0].current.skytext;
+        // console.log(result);
+        // console.log(weatherType);
+        return res.json(result);
+    });
 })
 
 app.get("/ebay", function(req, res) {
-	const params = {
-		'OPERATION-NAME': 'findItemsByKeywords',
-		'keywords': "mens casual"
-	};
+    const params = {
+        'OPERATION-NAME': 'findItemsByKeywords',
+        'keywords': "mens casual raincoats"
+    };
 
-	ebay.get('finding', params, function(err, data) {
-		if(err) throw err;
-		return res.json(data.findItemsByKeywordsResponse[0].searchResult[0].item);
-	})
+    ebay.get('finding', params, function(err, data) {
+        if (err) throw err;
+        return res.json(data.findItemsByKeywordsResponse[0].searchResult[0].item);
+    })
 })
 
 app.get("/about", function(req, res) {
-	res.sendFile(path.join(__dirname, "about.html"));
+    res.sendFile(path.join(__dirname, "about.html"));
 })
 
 app.listen(PORT, () => console.log(`Listening to ${PORT}`));

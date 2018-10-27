3c45eba837c09b55aef54e8558f87c36cf9392ed
unknown <aguirreh8@gmail.com>
2018-10-27 16:26:41 -0400

    Add weather, Ebay data into DOM

---
+++ keys.js
@@ -0,0 +1,5 @@
+exports.ebayKey = {
+  app_id: process.env.EBAY_APP_ID,
+  dev_id: process.env.EBAY_DEV_ID,
+  client_secret: process.env.EBAY_CLIENT_SECRET
+};
--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const weather = require('weather-js');
 const router = express.Router();
 const path = require("path");
-
+const keys = require("./keys")
+const Ebay = require("ebay");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+const ebay = new Ebay({
+	app_id: process.env.EBAY_APP_ID
+});
+
+app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, "index-design.html"));
 
 });
 
@@ -20,6 +26,22 @@ app.get("/api/:zip?", function(req, res) {
 	  // const weatherResults = JSON.stringify(result, null, 1);
 	  return res.json(result);
 	});
+})
+
+app.get("/ebay", function(req, res) {
+	const params = {
+		'OPERATION-NAME': 'findItemsByKeywords',
+		'keywords': "mens casual"
+	};
+
+	ebay.get('finding', params, function(err, data) {
+		if(err) throw err;
+		return res.json(data.findItemsByKeywordsResponse[0].searchResult[0].item);
+	})
+})
+
+app.get("/about", function(req, res) {
+	res.sendFile(path.join(__dirname, "about.html"));
 })
 
 app.listen(PORT, () => console.log(`Listening to ${PORT}`));

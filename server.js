fca6f459e728a8505946cce54ad200e70efab07f
unknown <aguirreh8@gmail.com>
2018-11-03 18:39:22 -0400

    added html routing files to replace app.get. Reconfigured package.json for glitch.com deployment

--- server.js
+++ server.js
@@ -4,28 +4,25 @@ const router = express.Router();
 const path = require("path");
 const keys = require("./keys")
 const Ebay = require("ebay");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+const routes = require('./routes/htmlRoutes.js');
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const ebay = new Ebay({
     app_id: process.env.EBAY_APP_ID
 });
-let weatherType = "";
 
 app.use(express.static('public'));
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index-design.html"));
-
-});
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+app.use(routes);
 
 app.get("/api/:zip?", function(req, res) {
     weather.find({ search: req.params.zip, degreeType: 'F' }, function(err, result) {
         if (err) console.log(err);
-        // weatherType = result[0].current.skytext;
-        // console.log(result);
-        // console.log(weatherType);
         return res.json(result);
     });
 })
@@ -40,10 +37,6 @@ app.get("/ebay", function(req, res) {
         if (err) throw err;
         return res.json(data.findItemsByKeywordsResponse[0].searchResult[0].item);
     })
-})
-
-app.get("/about", function(req, res) {
-    res.sendFile(path.join(__dirname, "about.html"));
 })
 
 app.listen(PORT, () => console.log(`Listening to ${PORT}`));
